799ecd78cea38c51faba29ce22556d8cd6282020
ramiro-e <ramiroestevez96@gmail.com>
2022-02-07 20:14:59 -0300

    Subiendo cambios

--- app.js
+++ app.js
@@ -23,51 +23,72 @@ async function checkCategories() {
 
 
         $('.categories__container', html).each( async function () {
-            let category =  lodash.kebabCase(lodash.deburr($(this).find('.categories__title a').text()))
+            let categoryName =  lodash.kebabCase(lodash.deburr($(this).find('.categories__title a').text()))
             $(this).children().find('.categories__item').each(async function(){
                 let subcategory = lodash.kebabCase(lodash.deburr($(this).find('a').text()))
-                
+                let subcatList
                 try {
                     let subcategoryFetch = await axios($(this).find('a').attr('href'));
                     console.log('CAT   ' + $(this).find('a').attr('href'))
-                    let res
-                    res = checkSubcategories (subcategoryFetch)
-                    
-                    while(res.moreSubcat == true){
-                        console.log(state)
-                    }
+                    subcatList = await checkSubcategories (subcategoryFetch)
                 } catch (error) {
-
+                    console.log("error @ checkCategories")
                 }
             })
         })
 
     } catch (error) {
-      console.log(error)
+        console.log(error)
     }
 }
+
+
 
 async function checkSubcategories (fetched){
     let html = fetched.data
+    let subcatList = {}
     $ = cheerio.load(html)
     if ($('.ui-search-filter-dl .ui-search-filter-dt-title:contains("Categorías")').length) {
-        console.log($('.ui-search-filter-dl .ui-search-filter-dt-title:contains("Categorías")').children().length)
-        $('.ui-search-filter-dl .ui-search-filter-dt-title:contains("Categorías")', html).children().find('.ui-search-link').each( async function(){
-            console.log('SUBCAT')
-            return {moreSubcat: true, url: $(this).attr('href')}
+        $('.ui-search-filter-dl .ui-search-filter-dt-title:contains("Categorías")').parent().children().find('.ui-search-filter-container').each( async function(){
+            let subcategoryName = lodash.kebabCase(lodash.deburr($(this).find('.ui-search-filter-name').text()))
+
+            try {
+                let url = await $(this).find('a').attr('href')
+                let fetchedData = await fetchSubcat(url) //<------ DOESN'T RETURN ANYTHING, IN FACT
+                console.log(fetchedData)                 //        IT STOPS EXECUTION ALL TOGETHER
+            } catch (error) {
+                console.log("error @ checkSubcategories")
+            }
+            
         })
+
     } else {
-        console.log('chau')
-        return {moreSubcat: false, url:$('.ui-search-styled-labelui-search-result__content ui-search-link .ui-search-styled-label')}
+        //functionThatSavesTheMostSoldLink()
     }
 }
 
-app.get('/', function (req, res) {
-    checkCategories()
-    res.json('This is my webscraper')
-})
+async function fetchSubcat(url){
+    try {
+        let subcategoryFetch = await axios(url); //<------ DOESN'T RETURN ANYTHING, IN FACT
+        let subcatList                           //        IT STOPS EXECUTION ALL TOGETHER
+        subcatList = await checkSubcategories (subcategoryFetch)
+        return subcatList
+    } catch (error) {
+        console.log('error @ fetchSubcat')
+    }
+}
+
 
 checkCategories()
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
 
+
+
+
+
+app.get('/', function (req, res) {
+    checkCategories()
+    res.json('Bienvenido a MasVendidosAPI - MercadoLibre Tracker')
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
